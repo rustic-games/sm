4e6c95336c2650410c297b7d983962f7f090d6ef
Jean Mertz <jean@mertz.fm>
2018-09-04 08:21:20 +0200

    docs(README): improve introduction paragraph

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 //! # 💋 SM – a static State Machine library
 //!
 //! SM allows you to define a collection of states and events using Rust's type
-//! system. Undefined behavior is not an option.
+//! system. You can query the current state, and execute transitions between
+//! states. State machine usage is validated at compile-time. Undefined behavior
+//! is not an option.
 //!
 //! The implementation ensures a zero-sized abstraction that uses Rust's
 //! type-system and ownership model to guarantee valid transitions between
