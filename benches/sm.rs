d45047007a4986bc776049321501c5ab3161badd
Jean Mertz <jean@mertz.fm>
2018-09-06 19:45:56 +0200

    test: make benchmark tests more representative

--- benches/sm.rs
+++ benches/sm.rs
@@ -31,34 +31,111 @@ fn criterion_benchmark(c: &mut Criterion) {
         |b| {
             b.iter(|| {
                 let sm = Machine::new(Idle);
-                let _ = sm.state();
+                assert_eq!(sm.state(), Idle);
 
                 let sm = sm.event(Simulate);
-                let _ = sm.state();
+                assert_eq!(sm.state(), Simulating);
 
                 let sm = sm.event(None);
-                let _ = sm.state();
+                assert_eq!(sm.state(), Idle);
 
                 let sm = sm.event(Render);
-                let _ = sm.state();
+                assert_eq!(sm.state(), Rendering);
 
                 let sm = sm.event(None);
-                let _ = sm.state();
+                assert_eq!(sm.state(), Idle);
             })
         },
     );
 
-    c.bench_function("GameLoop; match Idle, Simulating, Rendering", |b| {
+    c.bench_function("GameLoop – match; Idle => Simulating => Idle => Rendering => Idle", |b| {
         b.iter(|| {
-            let sm = Machine::new(Rendering);
+            let mut sm = Machine::new(Idle).as_enum();
 
-            match sm.state().as_enum() {
-                States::Idle => (),
-                States::Simulating => (),
-                States::Rendering => (),
-            }
+            {
+                sm = match sm {
+                    States::Idle(m) => {
+                        let m = m.event(Simulate);
+                        assert_eq!(m.state(), Simulating);
+                        m.as_enum()
+                    },
+                    States::Simulating(m) => m.event(None).as_enum(),
+                    States::Rendering(m) => m.event(None).as_enum(),
+                };
+
+                sm = match sm {
+                    States::Idle(m) => m.event(Simulate).as_enum(),
+                    States::Simulating(m) => {
+                        let m = m.event(None);
+                        assert_eq!(m.state(), Idle);
+                        m.as_enum()
+                    },
+                    States::Rendering(m) => m.event(None).as_enum(),
+                };
+
+                sm = match sm {
+                    States::Idle(m) => {
+                        let m = m.event(Render);
+                        assert_eq!(m.state(), Rendering);
+                        m.as_enum()
+
+                    },
+                    States::Simulating(m) => m.event(None).as_enum(),
+                    States::Rendering(m) => m.event(None).as_enum(),
+                };
+
+                let _ = match sm {
+                    States::Idle(m) => m.event(Simulate).as_enum(),
+                    States::Simulating(m) => m.event(None).as_enum(),
+                    States::Rendering(m) => {
+                        let m = m.event(None);
+                        assert_eq!(m.state(), Idle);
+                        m.as_enum()
+                    },
+                };
+            };
         })
     });
+
+    c.bench_function(
+        "GameLoop – loop; Idle => Simulating => Idle => Rendering => Idle",
+        |b| {
+            b.iter(|| {
+                let mut sm = Machine::new(Idle).as_enum();
+                let mut i = 0;
+
+                loop {
+                    sm = match sm {
+                        States::Idle(m) => {
+                            if i == 0 {
+                                i += 1;
+                                let m = m.event(Simulate);
+                                assert_eq!(m.state(), Simulating);
+                                m.as_enum()
+                            } else if i == 1 {
+                                i += 1;
+                                let m = m.event(Render);
+                                assert_eq!(m.state(), Rendering);
+                                m.as_enum()
+                            } else {
+                                break;
+                            }
+                        },
+                        States::Simulating(m) => {
+                            let m = m.event(None);
+                            assert_eq!(m.state(), Idle);
+                            m.as_enum()
+                        },
+                        States::Rendering(m) => {
+                            let m = m.event(None);
+                            assert_eq!(m.state(), Idle);
+                            m.as_enum()
+                        },
+                    }
+                }
+            })
+        },
+    );
 }
 
 criterion_group!(benches, criterion_benchmark);
